4d9dc326c6d36232bb940ac82c865d161daaab04
Samue Pilay Muñoz <yhau1989@gmail.com>
2019-08-13 16:57:07 -0500

    Se agrega mejoras a la página de inicio

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -9,9 +9,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="ui container">
       <div class="ui large secondary pointing menu hidden">
         <a class="item">Inicio</a>
-        <a class="item" href="#seccionApp">App</a>
-        <a class="item" href="#seccionEmpresa">Empresa</a>
-        <a class="item" href="#seccionProcesos">Procesos</a>
+        <a class="item" href="#seccionApp">Empresa</a>
+        <a class="item" href="#seccionEmpresa">App</a>
+        <a class="item" href="#seccionProcesos">Tecnologías</a>
         <div class="right item">
           <a href="<?php echo strtolower(base_url()); ?>ingreso" class="ui secondary button">Login</a>
         </div>
@@ -30,20 +30,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="ui middle aligned stackable grid container">
       <div class="row">
         <div class="eight wide column">
-          <h3 class="ui header">We Help Companies and Companions</h3>
-          <p>We can give your company superpowers to do things that they never thought possible. Let us
-            delight your customers and empower your needs...through pure data analytics.</p>
-          <h3 class="ui header">We Make Bananas That Can Dance</h3>
-          <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+          <h3 class="ui header">Soluciones para reciclaje</h3>
+          <p>Empresa Ecuatoriana con experiencia en el mercado de procesamiento de Fibra, la operación en Innomine S.A – Procefibras, se basa en el respeto del tiempo de nuestros proveedores y clientes.</p>
+          <h3 class="ui header">Compra de materiales</h3>
+          <p>En toda la provincia del Guayas, compramos cualquier tipo de material reciclable. 
+             <br><strong>Horario de atención:</strong> Lunes a Viernes de 8:00hrs a 18:00hrs, Sábados de 08:00 a 12:30
           </p>
         </div>
         <div class="six wide right floated column">
-          <img src="<?php echo strtolower(base_url()); ?>/assets/images/93e38a80576251.5d26f2bb67625.jpg" class="ui large bordered rounded image">
+          <img src="https://procefibras.com/wp-content/uploads/2019/05/cropped-IMG-20181016-WA0435-2.jpg" class="ui large bordered rounded image">
         </div>
       </div>
       <div class="row">
         <div class="center aligned column">
-          <a class="ui huge button">Check Them Out</a>
+          <a href="https://procefibras.com" target="_blank" rel="noopener noreferrer" class="ui huge button">Visitar sitio de la empresa</a>
         </div>
       </div>
     </div>
@@ -52,16 +52,47 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
   <div id="seccionEmpresa" class="ui vertical stripe quote segment">
     <div class="separ"></div>
-    <div class="ui equal width stackable internally celled grid">
+    <div class="ui stackable internally celled grid">
       <div class="center aligned row">
-        <div class="column">
-          <h3>"What a Company"</h3>
-          <p>That is what they all say about us</p>
+        <div class="four wide column">
+          <h3>Plataformas disponibles</h3>
+          <p>Android y IOS</p>
         </div>
-        <div class="column">
-          <h3>"I shouldn't have gone with their competitor."</h3>
+        <div class="twelve wide column">
+          <h3>Proceso de control</h3>
           <p>
-            <img class="ui avatar image"> <b>Nan</b> Chief Fun Officer Acme Toys
+            <div class="ui mini ordered tablet stackable steps">
+              <div class="active step">
+                <div class="content">
+                  <div class="title">Compra</div>
+                  <div class="description">Compra de chatarra</div>
+                </div>
+              </div>
+              <div class="active step">
+                <div class="content">
+                  <div class="title">Selección</div>
+                  <div class="description">Selección y división de chatarra</div>
+                </div>
+              </div>
+              <div class="active step">
+                <div class="content">
+                  <div class="title">Trituración</div>
+                  <div class="description">Triturar la chatarra seleccionada</div>
+                </div>
+              </div>
+              <div class="active step">
+                <div class="content">
+                  <div class="title">Almacenamiento</div>
+                  <div class="description">Almacenar la materia prima por colores</div>
+                </div>
+              </div>
+              <div class="active step">
+                <div class="content">
+                  <div class="title">Venta</div>
+                  <div class="description">Venta de materia prima</div>
+                </div>
+              </div>
+            </div>
           </p>
         </div>
       </div>
@@ -71,42 +102,48 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 
-  <div id="seccionProcesos" class="ui vertical stripe segment">
+  <div class="ui vertical stripe segment">
     <div class="separ"></div>
     <div class="ui text container">
-      <h3 class="ui header">Breaking The Grid, Grabs Your Attention</h3>
-      <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
-      <a class="ui large button">Read More</a>
+      <h3 class="ui header">Open source</h3>
+      <p>La aplicación mobil de Profecibras, esta constriuda 100% con tecnologías Open Source, usa lenguajes de programación como PHP, JavaScript y usa una base de datos
+      MySQL</p>
       <h4 class="ui horizontal header divider">
-        <a href="#">Case Studies</a>
+        <a href="#">Plataforma Tecnológica</a>
       </h4>
-      <h3 class="ui header">Did We Tell You About Our Bananas?</h3>
-      <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
-      <a class="ui large button">I'm Still Quite Interested</a>
+      <h3 class="ui header">Arquitectura de la Plataforma para el App</h3>
+      <p>
+        Para que el app funcione de manera optima y esté operativa 24/7 creo una arquitectura basada en 
+        <a href="https://es.wikipedia.org/wiki/Software_como_servicio" target="_blank" rel="noopener noreferrer">Sass</a> cuenta con un Api restfull 
+        (Servicio web) que permite la interacción con la base de datos, el App mobil se conecta a este servicio web el mismo que esta alojado en  
+        <a href=" https://cloud.google.com/" target="_blank" rel="noopener noreferrer">Google Cloud</a> el sitio que estamos viendo y la Bade de datos estan alojados en 
+        un Goole Cloud.       
+      </p>
     </div>
     <div class="separ"></div>
   </div>
 
-  <div class="ui inverted vertical footer segment">
+  <div id="seccionProcesos" class="ui inverted vertical footer segment">
     <div class="separ"></div>
     <div class="ui container">
       <div class="ui stackable inverted divided equal height stackable grid">
         <div class="three wide column">
           <h4 class="ui inverted header">Frameworks Usados</h4>
           <div class="ui inverted link list">
-            <a href="https://facebook.github.io/react-native/" class="item">React Native</a>
-            <a href="https://medoo.in/" class="item">Medoo</a>
-            <a href="https://www.codeigniter.com/" class="item">Codeigniter</a>
-            <a href="https://semantic-ui.com/" class="item">Semantic UI</a>
+            <a href="https://facebook.github.io/react-native/" class="item" target="_blank" rel="noopener noreferrer">React Native</a>
+            <a href="https://medoo.in/" class="item" target="_blank" rel="noopener noreferrer">Medoo</a>
+            <a href="https://www.codeigniter.com/" class="item" target="_blank" rel="noopener noreferrer">Codeigniter</a>
+            <a href="https://semantic-ui.com/" class="item" target="_blank" rel="noopener noreferrer">Semantic UI</a>
+            <a href="https://expo.io/" class="item" target="_blank" rel="noopener noreferrer">Expo</a>
           </div>
         </div>
         <div class="three wide column">
           <h4 class="ui inverted header">Tecnologias Usadas</h4>
           <div class="ui inverted link list">
-            <a href="https://www.mysql.com/" class="item">MySql</a>
-            <a href="https://php.net/" class="item">PHP</a>
-            <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" class="item">JavaScript</a>
-            <a href="https://cloud.google.com/" class="item">Google Cloud</a>
+            <a href="https://www.mysql.com/" target="_blank" rel="noopener noreferrer" class="item">MySql</a>
+            <a href="https://php.net/" target="_blank" rel="noopener noreferrer" class="item">PHP</a>
+            <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer" class="item">JavaScript</a>
+            <a href="https://cloud.google.com/" target="_blank" rel="noopener noreferrer" class="item">Google Cloud</a>
           </div>
         </div>
         <div class="seven wide column">
